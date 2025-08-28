d7c09700f6d959b2aae49fc0db68f5654d70db94
vishal kumar <iamvishalnigam@gmail.com>
2025-08-28 16:00:03 +0530

    Loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -34,7 +34,7 @@ const ExploreSkeleton = () => {
             <div className="h-4 w-72 bg-gray-200 rounded"></div>
           </div>
 
-          {/* Skeleton Grid */}
+          {/* Skeleton Grid for Posts */}
           <div className="grid grid-cols-3 gap-2 animate-pulse">
             {Array.from({ length: 9 }).map((_, i) => (
               <div
